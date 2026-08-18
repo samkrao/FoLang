aea894f1307cc68fcca6111210e05b75d192666c
samkrao <samkrao@gmail.com>
2026-08-18 20:38:30 +0545

    updated

---
+++ docs/build/merge-pdf.py
@@ -0,0 +1,11 @@
+from pypdf import PdfReader, PdfWriter
+
+writer = PdfWriter()
+
+for pdf in ["../cover_page_a4.pdf", "folang-language-reference.pdf"]:
+    reader = PdfReader(pdf)
+    for page in reader.pages:
+        writer.add_page(page)
+
+with open("language-ref.pdf", "wb") as f:
+    writer.write(f)
