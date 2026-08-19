01ad8348e56199a4fbc11a8cfcca7590ac9f0b27
samkrao <samkrao@gmail.com>
2026-08-19 18:05:51 +0545

    updated

--- docs/build/merge-pdf-book.py
+++ docs/build/merge-pdf-book.py
@@ -2,7 +2,7 @@ from pypdf import PdfReader, PdfWriter
 
 writer = PdfWriter()
 
-for pdf in ["cover/lrm_cover_a4.pdf", "book/tlfolang.pdf"]:
+for pdf in ["cover/cover_page_a4.pdf", "book/tlfolang.pdf"]:
     reader = PdfReader(pdf)
     for page in reader.pages:
         writer.add_page(page)
